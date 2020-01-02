e01bf358c757db5f3da3584cd12fad760de6639f
mskmeg <mskmeg@yahoo.com>
2020-01-02 10:41:33 -0500

    1

--- extensions/date-filter-default/script.js
+++ extensions/date-filter-default/script.js
@@ -1,15 +1,22 @@
-tableau.extensions.initializeAsync().then(() => {
-  let dashboard = tableau.extensions.dashboardContent.dashboard;
-  let selectedWorksheet = dashboard.worksheets.find(w => w.name === 'Historical Trend');
-//  let fieldName = 'Date';
-//  updateFilterRange(selectedWorksheet, fieldName);
+'use strict';
 
-  $("body").html("Hello 2<br>");
-});
+// Wrap everything in an anonymous function to avoid polluting the global namespace
+(function () {
+  $(document).ready(function () {
+    tableau.extensions.initializeAsync().then(() => {
+      let dashboard = tableau.extensions.dashboardContent.dashboard;
+      let selectedWorksheet = dashboard.worksheets.find(w => w.name === 'Historical Trend');
+      // let fieldName = 'Date';
+      // updateFilterRange(selectedWorksheet, fieldName);
 
-function updateFilterRange(worksheet, fieldName) {
-  let today = new Date();
-  let lastYear = new Date();
-  lastYear.setFullYear(today.getFullYear()-1);
-  worksheet.applyRangeFilterAsync(fieldName, { min: lastYear, max: today});
-}
+      $("body").html("Hello 3");
+    });
+  });
+
+  function updateFilterRange(worksheet, fieldName) {
+    let today = new Date();
+    let lastYear = new Date();
+    lastYear.setFullYear(today.getFullYear()-1);
+    worksheet.applyRangeFilterAsync(fieldName, { min: lastYear, max: today});
+  }
+})();
