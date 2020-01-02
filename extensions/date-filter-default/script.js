502f489102bad3b39c60fdbd7ac8f848a656c333
mskmeg <mskmeg@yahoo.com>
2020-01-02 09:45:08 -0500

    c

---
+++ extensions/date-filter-default/script.js
@@ -0,0 +1,13 @@
+tableau.extensions.initializeAsync().then(() => {
+  let dashboard = tableau.extensions.dashboardContent.dashboard;
+  let selectedWorksheet = dashboard.worksheets.find(w => w.name === 'Historical Trend');
+  let fieldName = 'Date';
+  updateFilterRange(selectedWorksheet, fieldName);
+});
+
+function updateFilterRange(worksheet, fieldName) {
+  let today = new Date();
+  let lastYear = new Date();
+  lastYear.setFullYear(today.getFullYear()-1);
+  worksheet.applyRangeFilterAsync(fieldName, { min: lastYear, max: today});
+}
